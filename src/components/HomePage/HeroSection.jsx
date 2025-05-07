629b68de61f7f8e1bff98c6389da1c4bd6c61101
Eason748 <flatlandapp@gmail.com>
2025-05-07 09:07:50 +0800

    up

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -28,7 +28,7 @@ function HeroSection() {
           {t('home:hero.subtitle')}
         </motion.p>
 
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.4 }}
@@ -47,7 +47,7 @@ function HeroSection() {
           >
             {t('buttons.learnMore')}
           </Link>
-        </motion.div>
+        </motion.div> */}
       </div>
     </section>
   );
