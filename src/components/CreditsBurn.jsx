39c085821d922a2c7e970947adb2660b8eb98c4b
Eason748 <flatlandapp@gmail.com>
2025-06-06 12:59:03 +0800

    up

--- src/components/CreditsBurn.jsx
+++ src/components/CreditsBurn.jsx
@@ -268,7 +268,7 @@ const CreditsBurn = ({ onClose }) => {
           <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-          <span>Exchange Rate: 1 Credit = {exchangeRate} CFX</span>
+          <span>Burn: 1 Credit, Reward {exchangeRate} CFX</span>
         </div>
       </div>
 
